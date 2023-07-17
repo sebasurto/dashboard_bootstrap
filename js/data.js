95e80ca2b0fce898f5ed57a0567c32efbace0baa
Sergio Basurto <35089326+sebasurto@users.noreply.github.com>
2023-07-17 07:43:40 -0500

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -17,26 +17,35 @@ let plot = (data) => {
         data: dataset,
         backgroundColor: 'rgba(255, 99, 132, 0.2)',
     };
-    /*const config = {
-        type: 'radar',
-        data: data,
-        options: {
-          elements: {
-            line: {
-              borderWidth: 3
-            }
-          }
-        },
-      };*/
     const chart = new Chart(ctx, config)
   
   };
-  let load = (data) => {
 
-  }
+  let plot2 = (data) => {
+    const ctx = document.getElementById('myChart3');
+    const dataset = {
+      labels: data.daily.uv_index_max, /* ETIQUETA DE DATOS */
+      datasets: [{
+          label: 'Indice Máximo de UV', /* ETIQUETA DEL GRÁFICO */
+          data: data.daily.uv_index_max, /* ARREGLO DE DATOS */
+          fill: false,
+          borderColor: 'rgb(255, 57, 192)', 
+          backgroundColor: 'blue',
+          tension: 0.1
+         
+      }]
+    };
+    
+    const config = {
+        type: 'bar',
+        data: dataset,
+        backgroundColor: 'rgba(120, 200, 132, 0.2)',
+    };
+    const chart = new Chart(ctx, config)
+  };
   let loadInocar = () => { 
   //let URL = 'https://www.inocar.mil.ec/mareas/consultan.php';
-  let URL_proxy = 'http://localhost:8081/' // Coloque el URL de acuerdo con la opción de proxy
+  let URL_proxy = 'https://cors-anywhere.herokuapp.com/' //'://localhost:80http81/' // Coloque el URL de acuerdo con la opción de proxy
   let URL = URL_proxy + 'https://www.inocar.mil.ec/mareas/consultan.php';
   fetch(URL)
        .then(response => response.text())
@@ -53,29 +62,27 @@ let plot = (data) => {
         .catch(console.error);
   }
 
-(
-    function () { 
-       
-      loadInocar();
 
-    }
-  
-  )();
 (
     function(){
+    let meteo = localStorage.getItem('meteo');
     let URL = 'https://api.open-meteo.com/v1/forecast?latitude=-2.20&longitude=-79.89&hourly=temperature_2m&daily=uv_index_max&current_weather=true&timezone=America%2FNew_York';
     fetch( URL )
     .then(response => response.json())
     .then(data => {
+        
         let timezone =data["timezone"]
         let timezoneHTML = document.getElementById("timezone")
         timezoneHTML.textContent = timezone;
         console.log(data);
-        plot(data)
-        load(data)
+        plot(data);
+        plot2(data);
+        localStorage.setItem("meteo", JSON.stringify(data))
+
         //let time = data []
     })
     .catch(console.error);
-    }
-)();
+    loadInocar();  
+  
+  })();
 
